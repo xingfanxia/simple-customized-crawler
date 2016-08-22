28fac6f26c9523f39d43b3b8038611a69fd342ce
xxf1995 <xiax@carleton.edu>
2016-08-22 22:32:17 +0800

    work on word parsing

--- urlExtractorFromXls.py
+++ urlExtractorFromXls.py
@@ -7,24 +7,42 @@
 # @Version : $3.0
 
 # import modules
-import xlrd,sys,re,urlhelper,os
+import xlrd,sys,re,urlhelper,os,docx2txt,io
 
 # Read data from Excel Cells and check if it is an URL
 # If so, write it into the Output file
 def read_and_save(filename):
-	book = xlrd.open_workbook(filename)
-	sh = book.sheet_by_index(0)
-	with open("Output.txt", "a") as my_file:
-		my_file.write(filename +"\n")
-		for rx in range(sh.nrows):
-			for cx in range(sh.ncols):
-				text = str(sh.cell_value(rowx=rx, colx =cx))
-				validation = re.findall(urlhelper.URL_REGEX, text)
+	if (".xls" in filename):
+		book = xlrd.open_workbook(filename)
+		sh = book.sheet_by_index(0)
+		with open("Output.txt", "a") as my_file:
+			my_file.write(filename +"\n")
+			for rx in range(sh.nrows):
+				for cx in range(sh.ncols):
+					text = str(sh.cell_value(rowx=rx, colx =cx))
+					validation = re.findall(urlhelper.URL_REGEX, text)
+					if (validation):
+						theURL = str(validation[0])
+						if ("http" not in theURL):
+							theURL = "http://" + theURL
+						my_file.write(theURL+"\n")
+	elif (".docx" in filename):
+		text = docx2txt.process(filename)
+		buf = io.StringIO(text)
+		lines = buf.readline()
+		print(text)
+		# pos = msg.find("\n")
+		# first_line = msg[:pos]
+		# ...
+		with open("Output.txt", "a") as my_file:
+			for l in lines:
+				validation = re.findall(urlhelper.URL_REGEX, l)
 				if (validation):
 					theURL = str(validation[0])
 					if ("http" not in theURL):
 						theURL = "http://" + theURL
-					my_file.write(theURL+"\n")
+					my_file.write(theURL+"\n")			
+
 
 # Main method, run $python3 urlExtractorFromXls.py filename.xls
 # You can put all the .xls files in the same directory and
